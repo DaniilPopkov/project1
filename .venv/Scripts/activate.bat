3339214cd5d826e4e06f83764b1d5d74af7482e2
DaniilPopkov <daniilpopkov66@gmail.com>
2025-04-03 11:42:11 +0300

    4

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kursk\OneDrive\Рабочий стол\Cafe\.venv
+set VIRTUAL_ENV=C:\Users\kursk\OneDrive\Рабочий стол\git_popkov\project1\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
